a67ea3dadf44a194fddcaea02e12a55811b96f01
Vaibhav <2002vaibhavlakhera3@gmail.com>
2024-12-04 13:35:20 +0530

    fix: remove path attribute

--- server/model.go
+++ server/model.go
@@ -7,9 +7,10 @@ import (
 type File struct {
     ID         int       `gorm:"primaryKey;autoIncrement"`
     Name       string    `gorm:"type:varchar(255);not null"`
-    Path       string    `gorm:"type:varchar(255);not null"`
     HashDigest string    `gorm:"type:varchar(256)"`
     Content    string    `gorm:"type:text;not null"`
     CreatedAt  time.Time `gorm:"type:datetime"`
     UpdatedAt  time.Time `gorm:"type:datetime"`
 }
+
+
