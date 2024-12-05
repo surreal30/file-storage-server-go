0180453496c5ae8aa1ffec1fea1989d30c24e81a
Vaibhav <2002vaibhavlakhera3@gmail.com>
2024-12-05 15:48:47 +0530

    test: create test for getWC()

---
+++ client/client_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func testGetWC(t *testing.T) {
+	// mock server
+	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/wc" {
+			http.Error(w, "Not Found", http.StatusNotFound)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintln(w, "All files contain 33 words")
+	}))
+	defer mockServer.Close()
+
+	serverURL := mockServer.URL + "/wc"
+
+	result, err := getWC(serverURL)
+	if result != "All files contain 33 words" && err == nil {
+		t.Errorf("Incorrect output")
+	}
+}
